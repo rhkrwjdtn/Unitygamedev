c109c24ccdf08302711376ea17a31f706182de65
DESKTOP-E7M0NKJ\kwonhyucknae <nhk321@naver.com>
2017-01-13 23:09:01 +0900

    정수가 1시간전에 올렷던걸로 다시 커밋 주식하기 복권하기 되고 버튼 위치들 바껴져잇음

--- Assets/Scripts/Employment/ButtonEnable.cs
+++ Assets/Scripts/Employment/ButtonEnable.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonEnable : MonoBehaviour {
-    public UnityEngine.UI.Button btn=null;
-    public Image image = null;
+    public UnityEngine.UI.Button btn;
     public int money=30;
-    public GameObject FirstJugall=null;
-    
-
 	// Use this for initialization
 	void Start () {
         if (btn == null)
         {
             btn = gameObject.GetComponent<UnityEngine.UI.Button>();
-            //Sprite newSprite = Resources.Load<Sprite>("jugall");
-            //image.overrideSprite = newSprite;
         }
             	
 	}
@@ -29,15 +23,7 @@ public class ButtonEnable : MonoBehaviour {
             ColorBlock cd = btn.colors;
             cd.normalColor = Color.blue;
             btn.colors = cd;
-        }
-        else
-        {
 
-        }
+        }	
 	}
-    public void ShowImage()
-    {
-        FirstJugall.active = true;
-        //image.enabled = true;
-    }
 }
