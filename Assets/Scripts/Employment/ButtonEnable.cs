6a37aee201b81c5c98da1cb2793f88552e58ee13
DESKTOP-E7M0NKJ\kwonhyucknae <nhk321@naver.com>
2017-01-13 13:34:46 +0900

    2017/1/13 버튼 비활성화까지

---
+++ Assets/Scripts/Employment/ButtonEnable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEnable : MonoBehaviour {
+    public UnityEngine.UI.Button btn;
+    public int money=30;
+	// Use this for initialization
+	void Start () {
+        if (btn == null)
+        {
+            btn = gameObject.GetComponent<UnityEngine.UI.Button>();
+        }
+            	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(money>15)
+        {
+            btn.enabled = false;
+            ColorBlock cd = btn.colors;
+            cd.normalColor = Color.blue;
+            btn.colors = cd;
+
+        }	
+	}
+}
