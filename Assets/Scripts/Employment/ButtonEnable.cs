348bbc49f72cf74b219f0d165d3ee3932aee6047
kwonhyucknae <nhk321@naver.com>
2017-01-15 14:23:24 +0900

    2017-1-15일3번째 버튼인 워렌버핏 알바를 고용하면 5초후에 보물상자를 생성하고 보물상자를 5번 클릭하면 보물상자가 사라지는거까지

--- Assets/Scripts/Employment/ButtonEnable.cs
+++ Assets/Scripts/Employment/ButtonEnable.cs
@@ -5,10 +5,22 @@ using UnityEngine.UI;
 
 public class ButtonEnable : MonoBehaviour {
     public UnityEngine.UI.Button btn;
-    public int money=30;
-    public int JugallumLevel;
+    public UnityEngine.UI.Button secondbtn;
+    public UnityEngine.UI.Button thirdbtn;
+    public int money=0;
+    public int randomnum = 0;
+    public int JugallumLevel=0;
+    public int touchcnt = 0;
     public GameObject Jugallum = null;
-    public Text btn_text;
+    public GameObject waren = null;
+    public GameObject box = null;
+    public GameObject goldbox = null;
+    public Text btn_text=null;
+    public bool waren_exist =false;
+    public bool box_exist = false;
+    public bool goldbox_exist = false;
+    public float timespan;
+    public float checkTime;
 
 	// Use this for initialization
 	void Start () {
@@ -17,23 +29,79 @@ public class ButtonEnable : MonoBehaviour {
             btn = gameObject.GetComponent<UnityEngine.UI.Button>();
 
         }
+        if(secondbtn==null)
+        {
+            secondbtn = gameObject.GetComponent<UnityEngine.UI.Button>();
+        }
+
         JugallumLevel = 0;
         PlayerPrefs.SetInt("FirstJugallumLevel", JugallumLevel);
         //btn_text.text = "test";
+
+        ColorBlock cd = btn.colors;
+        ColorBlock dd = btn.colors;
+        cd.normalColor = Color.blue;
+        dd.normalColor = Color.white;
+
+        btn.enabled = false;
+        secondbtn.enabled = false;
+        btn.colors = cd;
+        secondbtn.colors = cd;
+
+        timespan = 0.0f;
+        checkTime = 5.0f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	if(money>15)
+
+        Moneyupdate moneyu = GameObject.Find("MoneyManager").GetComponent<Moneyupdate>();
+        money = moneyu.money;
+
+        ColorBlock cd = btn.colors;
+        ColorBlock dd = btn.colors;
+        cd.normalColor = Color.blue;
+        dd.normalColor = Color.white;
+
+
+        if (money>101)
+        {
+            btn.enabled = true;
+            btn.colors = dd;
+        }	
+        else if(money<100)
         {
             btn.enabled = false;
-            ColorBlock cd = btn.colors;
-            cd.normalColor = Color.blue;
             btn.colors = cd;
+        }
+        if(money>301)
+        {
+            secondbtn.enabled = true;
+            secondbtn.colors = dd;
+        }
+        else if(money<300)
+        {
+            secondbtn.enabled = false;
+            secondbtn.colors = cd;
+        }
 
-        }	
+        if(waren_exist==true)
+        {
+            timespan += Time.deltaTime;
+            if(timespan>checkTime)
+            {
+                eventbox();
+                timespan = 0;
+            }
+            if (touchcnt > 5)
+            {
+                box.active = false;
+                goldbox.active = false;
+                touchcnt = 0;
+            }
 
-	}
+        }
+    }
     public void btnClick()
     {
         JugallumLevel = PlayerPrefs.GetInt("FirstJugallumLevel", 0);
@@ -42,9 +110,37 @@ public class ButtonEnable : MonoBehaviour {
         PlayerPrefs.SetInt("FirstJugallumLevel", JugallumLevel);
 
         btn_text.text= "레벨:"+JugallumLevel;
-
-        
-
     }   
 
+    public void secondbtnClick()
+    {
+        waren.active = true;
+        waren_exist = true;
+
+    }
+    public void eventbox()
+    {
+        randomnum = Random.Range(0, 10);
+        if(randomnum<6)
+        {
+            box.active = true;
+            box_exist = true;
+        }
+        else
+        {
+            goldbox.active = true;
+            goldbox_exist = true;
+        }
+
+
+    }
+    public void Touchbox()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            touchcnt++;
+
+        }
+    }
 }
