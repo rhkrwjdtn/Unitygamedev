008f5a005634b2c1c61ec8f9a0d2a986e8a9a2da
ParkDuri <2011305026@skuniv.ac.kr>
2017-01-19 09:27:52 +0900

    no message

---
+++ Assets/Scripts/House/HouseButtonEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseButtonEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setBG (int btn){
+		switch (btn) {
+		case 0:
+			Debug.Log ("btn0_onClick");
+		break;
+		case 1:
+			Debug.Log ("btn1_onClick");
+		break;
+		}
+	}
+}
