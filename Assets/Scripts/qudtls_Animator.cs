f3e4dfb3703aed3ff88d270eb8fcd71ec7027b7a
ParkDuri <2011305026@skuniv.ac.kr>
2017-02-03 04:09:42 +0900

    animation... ㅠㅠ

---
+++ Assets/Scripts/qudtls_Animator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qudtls_Animator : MonoBehaviour {
+	
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
