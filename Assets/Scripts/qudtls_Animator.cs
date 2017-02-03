24490bed56ddf852f2ccc4b74fb8dbf0d0af6e2f
kwakjeongsu <puffyteemo@gamil.com>
2017-02-03 17:49:02 +0900

    둥신 애니메이션 고추긁기

--- Assets/Scripts/qudtls_Animator.cs
+++ Assets/Scripts/qudtls_Animator.cs
@@ -1,19 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class qudtls_Animator : MonoBehaviour {
 	
-	private Animator animator;
+	public GameObject qudtls;
+	public Sprite[] qudtlsAni;
+
+
+	private int i;
 
 	// Use this for initialization
 	void Start()
-	{
-		animator = this.GetComponent<Animator>();
+	{	
+		i = 0;
+		qudtls.GetComponent<Image> ().sprite = qudtlsAni[0];
+
+		StartCoroutine ("QudtlsAnimation", 0.7);
 	}
+
+	IEnumerator QudtlsAnimation(float delayTime) {
+
+
 	
-	// Update is called once per frame
-	void Update () {
-		
+		if (i == 7) {
+			i=0;
+		} else if (i < 7) {
+			i++;
+		}
+
+		qudtls.GetComponent<Image> ().sprite = qudtlsAni [i];
+
+		yield return new WaitForSeconds(delayTime);
+		StartCoroutine("QudtlsAnimation", 0.7);
 	}
+
+
 }
